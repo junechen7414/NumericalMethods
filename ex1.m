44af1b6e2911b1c7689f1dd063ebcd5d81c168ef
Bobby <90739416+junechen7414@users.noreply.github.com>
2024-03-13 21:36:05 +0800

    HW3.5
    add homework3.5

--- ex1.m
+++ ex1.m
@@ -1,27 +1,25 @@
-k = 0;
-for i = 0:20
-    xb=incsearch(@(x) cos(2*x)+sin(6*x),1,6,k);
-    k = k +1;
-    disp(xb);
-end
-function xb = incsearch(func,xmin,xmax,ns)
-if nargin < 3 , error('at least 3 arguments required'), end
-if nargin<4,ns=50; end
+fprintf("term\tresult\t\tet.%%\t\tea,%%\n");
+IterMeth(0.5,0.05,50);
 
-x= linspace(xmin,xmax,ns);
-f= func(x);
-nb = 0; xb=[];
-for k=1:length(x)-1
-    if sign(f(k))~=sign(f(k+1))
-        nb=nb+1;
-        xb(nb,1)=x(k);
-        xb(nb,2)=x(k+1);
-    end
-end
-if isempty(xb)
-    disp('no brackets found');
-else
-    disp("number of brackets:");
-    disp(nb);      
+function IterMeth(x,es,maxit)
+%default values
+if nargin <2 | isempty(es) es=0.0001; end
+if nargin < 3 | isempty(maxit) maxit=50; end
+
+iter = 1 ; sol = 1 ; ea = 100; et=100;
+first = 1;
+firstet= (exp(x)-1)/exp(x)*100;
+fprintf("%d\t\t%f\t%f\n",iter,first,firstet);  
+while(1)
+    solold = sol ;
+    sol = sol + x^iter / factorial(iter);    
+    et = (exp(x)-sol)/exp(x)*100;
+    if sol~= 0 
+        ea = abs((sol-solold)/sol)*100;
+    end    
+    fx=sol;
+    iter = iter+1;    
+    fprintf("%d\t\t%f\t%f\t%f\n",iter,fx,et,ea);    
+    if ea<=es | iter>= maxit ,break,end
 end
 end
--- ex2.m
+++ ex2.m
@@ -1,27 +1,28 @@
-fm = @(m,cd,t,v) sqrt(9.81*m/cd)*tanh(sqrt(9.81*cd/m)*t)-v;
-fprintf('index\txl\t\t\txu\t\t\txr\t\t\tea\n');
-bisect(@(cd) fm(95,cd,9,46),0.2,0.5,0.1);
-function bisect(func,xl,xu,es,varargin)
-if nargin < 3 , error('at least 3 input arguments'),end
-test = func(xl,varargin{:}) * func(xu,varargin{:});
-if test > 0, error('no sign change'),end
-if nargin < 4 | isempty(es),es=0.0001;end
-iter = 0;xr=xl;ea = 100;
-fprintf("---bisection method---\n")
-while(1)
-    xrold=xr;
-    xr=(xl+xu)/2;    
-    if xr~=0,ea =abs((xr-xrold)/xr) * 100;end
-    test = func(xl,varargin{:})* func(xr,varargin{:});
-    fprintf('%d\t\t%f\t%f\t%f\t%f\n',iter+1,xl,xu,xr,ea);
-    iter = iter + 1;
-    if test < 0
-        xu=xr;
-    elseif test > 0
-        xl=xr;
-    else
-        ea=0;
-    end
-    if ea<=es  , break,end
-end
+cosine(0.9,8); %use sine function
+
+function cosine(x,index)
+%cosine approximate
+cosx = cos(pi/4); point5 = 0.5;et = abs((point5- cosx)*2)*100;
+fprintf("n\tf(x)^(n)\tf(pi/3) \tabs(et)\n");
+fprintf("0\tcosx\t\t%f\t%f\n",cosx,et);
+for i = 1 : 6          
+     switch(mod(i,4))
+         case 0 
+             cosx = cosx + cos(pi/4)* (pi/12)^i * factorial(i)^-1;
+             et = abs((point5- cosx)*2)*100;
+             fprintf("%d\tcosx\t\t%f\t%f\n",i,cosx,et);
+         case 1
+             cosx = cosx + -sin(pi/4)* (pi/12)^i * factorial(i)^-1;
+             et = abs((point5- cosx)*2)*100;
+             fprintf("%d\t-sinx\t\t%f\t%f\n",i,cosx,et);
+         case 2
+             cosx = cosx + -cos(pi/4)* (pi/12)^i * factorial(i)^-1;
+             et = abs((point5- cosx)*2)*100;
+             fprintf("%d\t-cosx\t\t%f\t%f\n",i,cosx,et);
+         case 3
+             cosx = cosx + sin(pi/4)* (pi/12)^i * factorial(i)^-1;
+             et = abs((point5- cosx)*2)*100;
+             fprintf("%d\tsinx\t\t%f\t%f\n",i,cosx,et);
+     end
+ end
 end
