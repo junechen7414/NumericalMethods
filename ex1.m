bf42b2d78cf4ebb968af4b5c34587397e88c1a41
Bobby <90739416+junechen7414@users.noreply.github.com>
2024-03-13 21:40:00 +0800

    HW9
    add homework9

--- ex1.m
+++ ex1.m
@@ -1,7 +1,28 @@
-clear,clc
-A = [2 -6 -1;-3 -1 7;-8 1 -2];
-b = [-38;-34;-40];
-[L,U,P] = LU_pivot(A);
-disp('L:'); disp(L);disp('U:'); disp(U);disp('P:'); disp(P);
-[d,x] = forward_back(L,U,b,P);
-disp('d:'); disp(d);disp('x:'); disp(x);
+x=[0 2 4 6 9 11 12 15 17 19];
+y=[5 6 7 6 9 8 8 10 12 12];
+[xp,yp] =linregr(x,y);
+subplot(2,1,1);
+plot(x,y,'o',xp,yp);
+grid on;
+%change x,y :
+[xp,yp] =linregr(y,x);
+subplot(2,1,2);
+plot(y,x,'o',xp,yp);
+grid on;
+function [xp,yp]= linregr(x,y)
+sumx=sum(x); sumy=sum(y);
+sumxy= sum(x.*y); sumxsqr = sum(x.^2);
+sumysqr = sum(y.^2);
+n = length(y);
+a(1) = (n*sumxy - sumx*sumy)/(n*sumxsqr - sumx^2); %slope
+a(2)= sumy/n - a(1)*mean(x); %intercept
+sr=0;
+for i= 1:n
+    sr = sr+ (y(i)-a(1)-a(2)*x(i))^2;    
+end
+syslashx = sqrt(sr/(n-2));
+r = (n*sumxy-sumx*sumy)/ sqrt(n*sumxsqr - sumx^2)/sqrt(n*sumysqr - sumy^2);
+fprintf('slope:%f\tintercept:%f\tstd:%f\tcorrelationcoefficient:%f\n',a(1),a(2),syslashx,r);
+xp = linspace(min(x),max(x),2);
+yp= a(1).*xp+a(2);
+end
--- ex2.m
+++ ex2.m
@@ -1,39 +1,17 @@
-clear,clc
-x = linspace(-5,5); y = linspace(-5,5);
-f = x.^2+y.^2-5;
-f2 = x.^2-y-1;
-plot3(x,y,f);
-hold on;
-plot3(x,y,f2);
-grid;
-
-x = [1.5;1.5];
-f =@(x) [x(1)^2+x(2)^2-5;x(1)^2-x(2)-1];
-f1 = @(y) sqrt(y+1);
-f2 = @(x) sqrt(5-x^2);
-J =@(x) [2*x(1) 2*x(2);2*x(1) -1];
-disp('-----successive substitution-----');
-fprintf('i\txold\t\tx\t\t\teax\t\t\tyold\t\ty\t\t\teay\n');
-succsub(f1,f2,x(1),x(2),0.1);
-disp('-----newtraph-----');
-newtmult(f,J,x,0.1);
-function newtmult(func1,func2,x0,es)
-iter = 0;
-x = x0;
-while(1)
-    f = func1(x);
-    J= func2(x);
-    dx = J\f;
-    x =x -dx;
-    iter = iter+1;
-    m= size(x);
-    fprintf('x\t\t\ty\n');
-    for i=1:m
-        fprintf('%f\t',x(i));
-    end
-    fprintf('\n');
-    ea = 100*max(abs(dx./x));    
-    fprintf('minea: %f\n',ea);
-    if ea<=es,break,end
-end
+x =[0.1 0.2 0.4 0.6 0.9 1.3 1.5 1.7 1.8];
+y=[0.75 1.25 1.45 1.25 0.85 0.55 0.35 0.28 0.18];
+a=linearize(x,y);
+alpha = exp(a(2));
+bata = a(1);
+fprintf('y= %f*x*exp(%f*x)\n',alpha,bata);
+function a= linearize(x,y)
+    sumx=sum(x);sumlogy=sum(log(y)-log(x));sumxx =sum(x.^2);
+    sumlogyy=sum((log(y)-log(x)).^2); sumlogyx= sum(x.*(log(y)-log(x)));
+    n = length(x); meanx = sumx/n; meany = sumlogy/n;
+    a(1) =(n*sumlogyx - sumx*sumlogy)/(n*sumxx - sumx^2);
+    a(2)= meany - a(1)*meanx;
+    xp = linspace(min(x),max(x));
+    yp= a(1).*xp+ a(2);
+    plot(x,log(y)-log(x),'o',xp,yp);
+    grid on;
 end
